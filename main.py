b3e538928e615aec9b8bb78470268f692ecc1ad2
Liza2203 <elizaveta-22-03@mail.ru>
2024-08-06 20:09:25 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+Number_of_completed_remote_sensing=12
+Number_of_hours_spent=1.5
+name_kyrs='Python'
+time=Number_of_hours_spent/Number_of_completed_remote_sensing
+print('Курс: ',name_kyrs+',',"всего задач:",Number_of_completed_remote_sensing,',',"затрачено часов: ",Number_of_hours_spent,',', 'среднее время выполнения ',time,'часа.')
